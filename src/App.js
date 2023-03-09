74b30365636ab7038315ce67a1e06da73f060359
yuj8fuj6 <yuj.m.fuj91@gmail.com>
2023-03-09 13:26:26 +0800

    Created React App

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          I hate Fucking Windows
+          Learn React
         </a>
       </header>
     </div>
