96618fbc5d98c88d67c537c7ef6916f8f763561f
yuj8fuj6 <yuj.m.fuj91@gmail.com>
2023-03-21 21:31:23 +0800

    Done Map Display

--- src/components/MapDisplay.jsx
+++ src/components/MapDisplay.jsx
@@ -1,9 +1,44 @@
-import React from 'react'
+import React, { useState, useContext } from "react";
+import { MapContainer, TileLayer, Popup, Marker } from "react-leaflet";
+import * as L from "leaflet";
+import { MapContext } from "../contexts/MapContext";
+
+const zoomLevel = 15;
 
 const MapDisplay = () => {
-  return (
-    <div className="text-darkgreen">MapDisplay</div>
-  )
-}
+  const { locationData, setLocationData } = useContext(MapContext);
 
-export default MapDisplay
+  const center = locationData;
+
+  const LeafIcon = L.Icon.extend({
+    options: {},
+  });
+
+  const redIcon = new LeafIcon({
+    iconUrl:
+      "https://chart.googleapis.com/chart?chst=d_map_xpin_icon_withshadow&chld=pin_star|home|FF0000|FFFF00",
+  });
+
+  return (
+    <div className="text-darkgreen">
+      <MapContainer
+        center={center}
+        zoom={zoomLevel}
+        scrollWheelZoom={true}
+        className="w-[1200px] h-[700px] rounded-xl ml-20 mt-10"
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker position={center} icon={redIcon}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+      </MapContainer>
+    </div>
+  );
+};
+
+export default MapDisplay;
--- src/components/MapSearch.jsx
+++ src/components/MapSearch.jsx
@@ -1,7 +1,90 @@
-import React from 'react'
+import React, { useState, useEffect, useContext } from "react";
+import Button from "./Button";
+import { Formik, Form, Field } from "formik";
+import { MapContext } from "../contexts/MapContext";
 
 const MapSearch = () => {
-  return <div className="text-darkgreen">MapSearch</div>;
-}
+  const { locationData, setLocationData } = useContext(MapContext);
 
-export default MapSearch
+  return (
+    <div className="text-darkgreen">
+      {/* <Formik
+        initialValues={initialValues}
+        onSubmit={(values, { resetForm }) => {
+          handleSubmit(values);
+          resetForm();
+        }}
+      >
+        {(props) => {
+          console.log(props.values);
+          return (
+            <Form className="grid grid-cols-1">
+              <label htmlFor="code" className="text-sm font-bold mt-4">
+                Course Code
+              </label>
+              <select
+                type="text"
+                id="code"
+                name="code"
+                className="border-darkgrey border-1 rounded text-sm font-normal indent-3"
+                value={props.values.short || props.values.code}
+                onChange={props.handleChange}
+              >
+                {allCourseData &&
+                  filteredCourses.map((course) => (
+                    <option
+                      value={course.course_code}
+                      label={course.course_code}
+                    >
+                      {course.course_code}
+                    </option>
+                  ))}
+              </select>
+              <label className="text-sm font-bold mt-4">Course Title</label>
+              <select
+                type="text"
+                id="name"
+                name="name"
+                className="border-darkgrey border-1 rounded text-sm font-normal indent-3"
+                value={props.values.short || props.values.name}
+                onChange={props.handleChange}
+              >
+                {allCourseData &&
+                  filteredCourses.map((course) => (
+                    <option
+                      value={course.course_name}
+                      label={course.course_name}
+                    >
+                      {course.course_name}
+                    </option>
+                  ))}
+              </select>
+              <label className="text-sm font-bold mt-4">
+                Course Description
+              </label>
+              <textarea
+                type="text"
+                id="content"
+                name="content"
+                className="border-darkgrey border-1 rounded h-[125px] text-sm font-normal p-3"
+                value={props.values.short || props.values.content}
+                onChange={props.handleChange}
+                placeholder="Post your content here!"
+              />
+              <div className="flex flex-row justify-center">
+                <button
+                  className="bg-darkgrey rounded-full border-1 p-1 text-yellow font-bold px-3 mt-4 hover:bg-yellow hover:text-darkgrey"
+                  type="submit"
+                >
+                  Confirm
+                </button>
+              </div>
+            </Form>
+          );
+        }}
+      </Formik> */}
+    </div>
+  );
+};
+
+export default MapSearch;
