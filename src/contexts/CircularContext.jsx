2706bf934e4994236f77a7b0be13808b9ad96e1f
yuj8fuj6 <yuj.m.fuj91@gmail.com>
2023-03-31 02:29:07 +0800

    Started circular context provider

--- src/contexts/CircularContext.jsx
+++ src/contexts/CircularContext.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React, { createContext, useState, useEffect } from "react";
+import axios from "axios";
 
-const CircularContext = () => {
+import { BACKEND_SCRAPER_URL } from "../constants.js";
+
+export const CircularContext = createContext();
+
+export const CircularContextProvider = (props) => {
+  const [uraData, setUraData] = useState([]);
+  const [bcaData, setBcaData] = useState([]);
+  const [scdfData, setScdfData] = useState([]);
+
   return (
-    <div>CircularContext</div>
-  )
-}
-
-export default CircularContext
+    <CircularContext.Provider value={{ uraData, bcaData, scdfData }}>
+      {props.children}
+    </CircularContext.Provider>
+  );
+};
