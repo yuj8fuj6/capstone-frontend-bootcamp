c805958262b026c34dece0e91afad81454da0409
yuj8fuj6 <yuj.m.fuj91@gmail.com>
2023-03-31 11:45:52 +0800

    Completed circular context

--- src/contexts/CircularContext.jsx
+++ src/contexts/CircularContext.jsx
@@ -10,6 +10,18 @@ export const CircularContextProvider = (props) => {
   const [bcaData, setBcaData] = useState([]);
   const [scdfData, setScdfData] = useState([]);
 
+  useEffect(() => {
+    axios.get(`${BACKEND_SCRAPER_URL}/circulars/ura`).then((res) => {
+      setUraData(res.data);
+    });
+    axios.get(`${BACKEND_SCRAPER_URL}/circulars/bca`).then((res) => {
+      setBcaData(res.data);
+    });
+    axios.get(`${BACKEND_SCRAPER_URL}/circulars/scdf`).then((res) => {
+      setScdfData(res.data);
+    });
+  }, []);
+
   return (
     <CircularContext.Provider value={{ uraData, bcaData, scdfData }}>
       {props.children}
