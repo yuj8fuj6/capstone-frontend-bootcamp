b5db9c48bfe31091ef147951609131eae7591390
yuj8fuj6 <yuj.m.fuj91@gmail.com>
2023-04-20 01:43:35 +0900

    Done CSS for Modal Checklist

--- src/components/ModalChecklist.jsx
+++ src/components/ModalChecklist.jsx
@@ -44,9 +44,60 @@ const ModalChecklist = (props) => {
         onOk={() => setOpenModal(false)}
         onCancel={() => setOpenModal(false)}
         centered={true}
-        width="700px"
+        width="1200px"
+        className="text-darkgreen"
       >
-        Hello!
+        <div className="text-base font-bold text-left">Checklist</div>
+        <div className="grid grid-cols-3 gap-6 mt-2">
+          <div>
+            <div className="text-right text-xs mb-1">
+              5/14 Items Outstanding
+            </div>
+            <div className="border-1 border-darkgreen rounded-xl p-2 items-center">
+              <div className="font-bold">URA</div>
+              <div className="h-[800px] overflow-auto">
+                <div className="grid grid-cols-12 items-center">
+                  <div className="col-span-1 font-bold">1.</div>
+                  <div className="col-span-11 border-1 border-slate-300 rounded-xl p-2">
+                    Hello
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div>
+            <div className="text-right text-xs mb-1">
+              5/14 Items Outstanding
+            </div>
+            <div className="border-1 border-darkgreen rounded-xl p-2 items-center">
+              <div className="font-bold">BCA</div>
+              <div className="h-[800px] overflow-auto">
+                <div className="grid grid-cols-12 items-center">
+                  <div className="col-span-1 font-bold">1.</div>
+                  <div className="col-span-11 border-1 border-slate-300 rounded-xl p-2">
+                    Hello
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div>
+            <div className="text-right text-xs mb-1">
+              5/14 Items Outstanding
+            </div>
+            <div className="border-1 border-darkgreen rounded-xl p-2 items-center">
+              <div className="font-bold">SCDF</div>
+              <div className="h-[800px] overflow-auto">
+                <div className="grid grid-cols-12 items-center">
+                  <div className="col-span-1 font-bold">1.</div>
+                  <div className="col-span-11 border-1 border-slate-300 rounded-xl p-2">
+                    Hello
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </Modal>
     </>
   );
