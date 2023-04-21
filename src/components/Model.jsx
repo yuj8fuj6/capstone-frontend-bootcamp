00e865e29ccc73e1b704cd47823b510eb380cbde
yuj8fuj6 <yuj.m.fuj91@gmail.com>
2023-04-21 16:19:20 +0900

    Added annotations to industrial building

--- src/components/Model.jsx
+++ src/components/Model.jsx
@@ -1,15 +1,21 @@
 import React, { Suspense, useContext } from "react";
-import { motion } from "framer-motion";
 import { Canvas } from "@react-three/fiber";
-import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
+import {
+  OrbitControls,
+  Preload,
+  useGLTF,
+  PresentationControls,
+} from "@react-three/drei";
 import { ModelContext } from "../contexts/ModelContext";
+import Annotation from "./ModelAnnotation";
+import { WarningOutlined } from "@ant-design/icons";
 
 import CanvasLoader from "./CanvasLoader";
 
 const Model = () => {
   const { modelType, setModelType } = useContext(ModelContext);
 
-  const industrialModel = useGLTF("./models/Industrial(Trial).glb");
+  const industrialModel = useGLTF("./models/Industrial.glb");
   const houseModel = useGLTF("./models/House.glb");
   const communityModel = useGLTF("./models/Community.glb");
 
@@ -17,10 +23,10 @@ const Model = () => {
     <>
       {modelType === "Industrial" && (
         <mesh>
-          <hemisphereLight intensity={0.15} groundColor="black" />
+          <hemisphereLight intensity={0.8} groundColor="black" />
           <pointLight intensity={1} />
           <spotLight
-            position={[-20, 50, 10]}
+            position={[-500, 2000, 500]}
             angle={0.12}
             penumbra={1}
             intensity={1}
@@ -29,9 +35,89 @@ const Model = () => {
           />
           <primitive
             object={industrialModel.scene}
-            position={[0, -3.25, -1.5]}
+            position={[-20, 0, 10]}
             scale={0.75}
           />
+          <Annotation position={[-20, 5, -30]}>
+            <div className="text-xs w-[200px] grid grid-cols-6">
+              <WarningOutlined />
+              <div className="col-span-5">
+                <p className="font-bold text-xxs">URA - Handbook</p>
+                <p className="font-bold text-xxs">Clause</p>
+                <p>Green buffer/ road reserve</p>
+              </div>
+            </div>
+          </Annotation>
+          <Annotation position={[-10, 5, -80]}>
+            <div className="text-xs w-[200px] grid grid-cols-6">
+              <WarningOutlined />
+              <div className="col-span-5">
+                <p className="font-bold text-xxs">SCDF - Fire Code</p>
+                <p className="font-bold text-xxs">Clause</p>
+                <p>Fire command center</p>
+              </div>
+            </div>
+          </Annotation>
+          <Annotation position={[210, 10, 3]}>
+            <div className="text-xs w-[200px] grid grid-cols-6">
+              <WarningOutlined />
+              <div className="col-span-5">
+                <p className="font-bold text-xxs">SCDF - Fire Code</p>
+                <p className="font-bold text-xxs">Clause</p>
+                <p>Unprotected opening calculation</p>
+              </div>
+            </div>
+          </Annotation>
+          <Annotation position={[50, 5, 10]}>
+            <div className="text-xs w-[200px] grid grid-cols-6">
+              <WarningOutlined />
+              <div className="col-span-5">
+                <p className="font-bold text-xxs">URA - Handbook</p>
+                <p className="font-bold text-xxs">Clause</p>
+                <p>Peripheral planting</p>
+              </div>
+            </div>
+          </Annotation>
+          <Annotation position={[20, 10, -50]}>
+            <div className="text-xs w-[200px] grid grid-cols-6">
+              <WarningOutlined />
+              <div className="col-span-5">
+                <p className="font-bold text-xxs">
+                  BCA - Code in Accessibility
+                </p>
+                <p className="font-bold text-xxs">Clause</p>
+                <p>Sheltered no floor level change</p>
+              </div>
+            </div>
+          </Annotation>
+          <Annotation position={[70, 10, -100]}>
+            <div className="text-xs w-[200px] grid grid-cols-6">
+              <WarningOutlined />
+              <div className="col-span-5">
+                <p className="font-bold text-xxs">BCA - Approved Document</p>
+                <p className="font-bold text-xxs">Clause</p>
+                <p>Staircase</p>
+              </div>
+            </div>
+            <div className="text-xs w-[200px] grid grid-cols-6 mt-3">
+              <WarningOutlined />
+              <div className="col-span-5">
+                <p className="font-bold text-xxs">SCDF - Fire Code</p>
+                <p className="font-bold text-xxs">Clause</p>
+                <p>Staircase</p>
+              </div>
+            </div>
+          </Annotation>
+          <Annotation position={[180, 5, -100]}>
+            <div className="text-xs w-[200px] grid grid-cols-6">
+              <WarningOutlined />
+              <div className="col-span-5">
+                <p className="font-bold text-xxs">SCDF - Fire Code</p>
+                <p className="font-bold text-xxs">Clause</p>
+                <p>Fire Engine Accessway</p>
+              </div>
+            </div>
+          </Annotation>
         </mesh>
       )}
       {modelType === "Residential" && (
@@ -49,7 +135,7 @@ const Model = () => {
           <primitive
             object={houseModel.scene}
             position={[0, -3.25, -1.5]}
-            scale={0.75}
+            scale={8.0}
           />
         </mesh>
       )}
@@ -68,7 +154,7 @@ const Model = () => {
           <primitive
             object={communityModel.scene}
             position={[0, -3.25, -1.5]}
-            scale={0.75}
+            scale={2.0}
           />
         </mesh>
       )}
@@ -81,16 +167,22 @@ const ModelCanvas = () => {
     <Canvas
       frameloop="demand"
       shadows
-      camera={{ position: [20, 3, 5], fov: 25 }}
+      camera={{ position: [-50, 100, 100], fov: 50 }}
+      dpr={[1, 2]}
       gl={{ preserveDrawingBuffer: true }}
     >
       <Suspense fallback={<CanvasLoader />}>
         <OrbitControls
           enableZoom={true}
-          // maxPolarAngle={Math.PI / 2}
-          // minPolarAngle={Math.PI / 2}
+          minDistance={50}
+          maxDistance={500}
+          makeDefault
+          minPolarAngle={0}
+          maxPolarAngle={Math.PI / 2}
         />
-        <Model />
+        <PresentationControls snap global>
+          <Model />
+        </PresentationControls>
       </Suspense>
       <Preload all />
     </Canvas>
